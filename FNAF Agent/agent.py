fe5c3739eefc13889d9a71583294d343108220ac
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-11-23 01:36:41 -0800

    added loading model

--- FNAF Agent/agent.py
+++ FNAF Agent/agent.py
@@ -6,6 +6,7 @@ import data
 from model import Linear_QNet, QTrainer
 import time
 import threading
+import os
 from helper import plot
 
 #from helper import plot
@@ -19,7 +20,7 @@ from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
-LR = 0.001
+LR = 0.00125
 
 class Agent:
     def __init__(self): #create the agent (need number of games, epsilon value (80 or 90 idk yet))
@@ -29,6 +30,15 @@ class Agent:
         self.memory = deque(maxlen=MAX_MEMORY) #popleft() called after deque becomes full
         self.model = Linear_QNet()
         self.trainer = QTrainer(self.model, lr=LR, gamma = self.gamma)
+
+    def load_model(self, model_path):
+        print(f"Loading model from: {model_path}")
+        try:
+            self.model.load_state_dict(torch.load(model_path))
+            self.model.eval()  # Set the model to evaluation mode
+            print("Model loaded successfully.")
+        except Exception as e:
+            print(f"Error loading the model: {e}")
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done)) #popleft() if MAX_MEMORY is reached
@@ -48,7 +58,7 @@ class Agent:
 
     def get_action(self, state):
         #random moves: tradeoff between exploration and exploitation
-        self.epsilon = 80 - self.num_games
+        self.epsilon = 300 - self.num_games
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0,16)
         else:
@@ -61,49 +71,50 @@ def train():
     plot_scores = []
     plot_mean_scores = []
     agent = Agent()
-    #data.create_host()
+
+    # Load the pre-trained model
+    model_path = r"C:\Users\mibbd\OneDrive\Desktop\FNAF\FNAF Agent\model\model.pth"
+    if os.path.exists(model_path):
+        agent.model.load(model_path)
 
     def on_connection_established(client_socket):
         total_score = 0
-        record = 0
+        record = agent.model.record_score  # Load the record from the model
         print("Connection established")
+
         while True:
             time.sleep(.3)
-            #get old state
+            # get old state
             state_old = data.get_state()
-            #get move
+            # get move
             final_move = agent.get_action(state_old)
-            #preform move and get new state
+            # perform move and get new state
             reward, done, score = data.play_step(final_move)
-            
+
             state_new = data.get_state()
-            #train short memory (1 step)
+            # train short memory (1 step)
             agent.train_short_memory(state_old, final_move, reward, state_new, done)
-            #remember
+            # remember
             agent.remember(state_old, final_move, reward, state_new, done)
-            if(done):
-                #train long memory and plot result
+            if done:
+                # train long memory and plot result
                 data.reset()
-                agent.num_games+=1
+                agent.num_games += 1
                 agent.train_long_memory()
 
                 if score > record:
                     record = score
-                    agent.model.save() #when model is created
-                
+                    agent.model.record_score = record
+                    agent.model.save()  # Save the model with updated record
+
                 print("Game", agent.num_games, 'Score', score, "Record:", record)
-                #plot matplotlib when you install it bud stfu 
-                #1:46 am 11/4/23
-                #stfu
-                #9:51pm 11/10/23 
-                #L
                 plot_scores.append(score)
                 total_score += score
                 mean_score = total_score / agent.num_games
                 plot_mean_scores.append(mean_score)
                 plot(plot_scores, plot_mean_scores)
-    data.create_host(on_connection_established)
 
+    data.create_host(on_connection_established)
 
 
 if __name__ == "__main__":
